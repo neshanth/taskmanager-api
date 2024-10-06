e915b499f903eb54256a83f90223fb6d2ea8db9a
neshanth <59483685+neshanth@users.noreply.github.com>
2024-10-06 09:17:39 +0530

    removed comment

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,6 @@ class Kernel extends ConsoleKernel
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
-    // Cron scheduler functions
     protected function schedule(Schedule $schedule)
     {
         $schedule->call(function(){
